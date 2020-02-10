d763e647f9c3a4d0c2e98dd952ca03e5e60c9a71
Colin Smith <smith.colin00@gmail.com>
2020-02-09 22:35:32 -0700

    wip

---
+++ Pizza Time/Assets/TelephoneController.cs
@@ -0,0 +1,44 @@
+﻿using PizzaTime.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TelephoneController : MonoBehaviour
+{
+    public Sprite WithReciever;
+    public Sprite WithoutReciever;
+    private Image Image;
+    private bool RecieverHungUp = true;
+    private Telephone Telephone;
+
+    private void Awake()
+    {
+        Image = GetComponent<Image>();
+        Telephone = new Telephone(null);
+    }
+
+    private void Update()
+    {
+        HandleRecieverSwap();
+        if (Telephone.IsRinging)
+        {
+            Debug.Log("TELHPONE RIGING");
+        }
+    }
+
+    private void HandleRecieverSwap()
+    {
+        if (RecieverHungUp && Image.sprite == WithoutReciever)
+        {
+            Image.sprite = WithReciever;
+        }
+        else if (!RecieverHungUp && Image.sprite == WithReciever)
+        {
+            Image.sprite = WithoutReciever;
+        }
+    }
+
+    public void ToggleReciever()
+    {
+        RecieverHungUp = !RecieverHungUp;        
+    }
+}
