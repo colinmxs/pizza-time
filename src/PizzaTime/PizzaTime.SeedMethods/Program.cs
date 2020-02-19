9a29f5da506b60a790457bfbf7b01fdf5f006b03
Colin Smith <smith.colin00@gmail.com>
2020-02-18 22:34:17 -0700

    add seedmethods project

---
+++ src/PizzaTime/PizzaTime.SeedMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PizzaTime.SeedMethods
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //await Conversation();
+            var customerSeed = new SeedCustomers();
+            customerSeed.AmountToSeed = 100;
+            foreach (var item in await customerSeed.Seed())
+            {
+                Console.WriteLine($"{item.FirstName} {item.LastName}, :: {item.Address}");
+            }
+        }
+    }
+}
---
+++ src/PizzaTime/PizzaTime.SeedMethods/SeedCustomers.cs
@@ -0,0 +1,56 @@
+﻿using CodenameGenerator;
+using PizzaTime.Core.Customers;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PizzaTime.SeedMethods
+{
+    public class SeedCustomers
+    {
+        public int AmountToSeed { get; set; }
+        private readonly Generator _generator;
+        private readonly Random _random;
+        private readonly string _areaCode;
+        private readonly string _zipCode;
+
+        private WordBank RoadSuffixes = new WordBank(Word.Noun, "RoadSuffixes", new WordRepository(new string[] { "Road", "Street", "Plaza", "Way", "Avenue", "Drive", "Lane", "Grove", "Gardens", "Place" }));
+
+
+        public SeedCustomers()
+        {
+            _generator = new Generator();
+            _random = new Random();
+            _zipCode = _random.Next(10000, 99999).ToString();
+            _areaCode = _random.Next(0, 999).ToString().PadRight(3, '0');            
+        }
+
+        public async Task<IEnumerable<Customer>> Seed()
+        {
+            var customers = new List<Customer>();
+
+            for (int i = 0; i < AmountToSeed; i++)
+            {
+                customers.Add(SeedCustomer());
+            }
+            return customers;
+        }
+
+        private Customer SeedCustomer()
+        {
+            _generator.SetParts(WordBank.FirstNames);
+            _generator.Casing = Casing.PascalCase;
+            var firstName = _generator.Generate();
+            _generator.SetParts(WordBank.LastNames);
+            _generator.Casing = Casing.PascalCase;
+            var lastName = _generator.Generate();
+            _generator.SetParts(WordBank.Nouns, RoadSuffixes);
+            _generator.Casing = Casing.PascalCase;
+            return new Customer(firstName, lastName, $"{_areaCode}-{_random.Next(0, 9999999).ToString().PadRight(7, '0')}") 
+            {
+                Address = $"{_areaCode} {_generator.Generate()}",
+                ZipCode = _zipCode
+            };
+        }
+    }
+}
