a62cb33195eaad786d3e75784dec9245dd673f30
Colin Smith <smith.colin00@gmail.com>
2020-02-24 22:43:36 -0700

    add ingredients and pizzas

---
+++ src/PizzaTime/PizzaTime.Core/Food/PizzaIngredients.cs
@@ -0,0 +1,27 @@
+﻿namespace PizzaTime.Core.Food
+{
+    using PizzaTime.Core.Food.Core;
+
+    public static class PizzaIngredients
+    {
+        public static readonly PizzaIngredient Pepperoni = new PizzaIngredient("Pepperoni", 2.50M);
+        public static readonly PizzaIngredient Mushrooms = new PizzaIngredient("Mushrooms", 1.25M);
+        public static readonly PizzaIngredient ArtichokeHearts = new PizzaIngredient("Artichoke Hearts", 2.50M);
+        public static readonly PizzaIngredient BellPeppers = new PizzaIngredient("Bell Peppers", 1.25M);
+        public static readonly PizzaIngredient CanadianBacon = new PizzaIngredient("Canadian Bacon", 2.50M);
+        public static readonly PizzaIngredient Ham = new PizzaIngredient("Ham", 1.25M);
+        public static readonly PizzaIngredient Salami = new PizzaIngredient("Salami", 2.50M);
+        public static readonly PizzaIngredient Sausage = new PizzaIngredient("Sausage", 1.25M);
+        public static readonly PizzaIngredient Beef = new PizzaIngredient("Beef", 2.50M);
+        public static readonly PizzaIngredient Chicken = new PizzaIngredient("Chicken", 1.25M);
+        public static readonly PizzaIngredient Bacon = new PizzaIngredient("Bacon", 2.50M);
+        public static readonly PizzaIngredient Onions = new PizzaIngredient("Onions", 1.25M);
+        public static readonly PizzaIngredient GreenOnions = new PizzaIngredient("Green Onions", 1.25M);
+        public static readonly PizzaIngredient Olives = new PizzaIngredient("Olives", 2.50M);        
+        public static readonly PizzaIngredient Pineapple = new PizzaIngredient("Pineapple", 2.50M);
+        public static readonly PizzaIngredient Tomatoes = new PizzaIngredient("Tomatoes", 1.25M);
+        public static readonly PizzaIngredient Jalapenos = new PizzaIngredient("Jalapenos", 2.50M);
+        public static readonly PizzaIngredient Spinach = new PizzaIngredient("Spinach", 1.25M);
+        public static readonly PizzaIngredient Garlic = new PizzaIngredient("Fresh Garlic", 1.25M);
+    }
+}
---
+++ src/PizzaTime/PizzaTime.Core/Food/Pizzas.cs
@@ -0,0 +1,105 @@
+﻿using PizzaTime.Core.Food.Core;
+using System.Collections.Generic;
+
+namespace PizzaTime.Core.Food
+{
+    public static class Pizzas
+    {
+        public static readonly Pizza Pepperoni = new Pizza(new List<PizzaIngredient>()
+        {
+            PizzaIngredients.Pepperoni
+        })
+        { 
+            Name = "Pepperoni"
+        };
+
+        public static readonly Pizza Hawaiian = new Pizza(new List<PizzaIngredient>()
+        {
+            PizzaIngredients.CanadianBacon,
+            PizzaIngredients.Pineapple
+        })
+        {
+            Name = "Hawaiian"
+        };
+
+        public static readonly Pizza Cheese = new Pizza(new List<PizzaIngredient>()
+        {
+        })
+        {
+            Name = "Cheese"
+        };
+
+        public static readonly Pizza Combination = new Pizza(new List<PizzaIngredient>()
+        {
+            PizzaIngredients.BellPeppers,
+            PizzaIngredients.Onions,
+            PizzaIngredients.Olives,
+            PizzaIngredients.Mushrooms,
+            PizzaIngredients.Pepperoni,
+            PizzaIngredients.Sausage,
+            PizzaIngredients.Ham,
+            PizzaIngredients.Salami
+        })
+        {
+            Name = "Combination"
+        };
+
+        public static readonly Pizza Meat = new Pizza(new List<PizzaIngredient>()
+        {
+            PizzaIngredients.Pepperoni,
+            PizzaIngredients.Sausage,
+            PizzaIngredients.Ham,
+            PizzaIngredients.Salami,
+            PizzaIngredients.Beef,
+            PizzaIngredients.CanadianBacon,
+            PizzaIngredients.Bacon
+        })
+        {
+            Name = "Meat Lovers"
+        };
+
+        public static readonly Pizza Vegetarian = new Pizza(new List<PizzaIngredient>()
+        {
+            PizzaIngredients.ArtichokeHearts,
+            PizzaIngredients.BellPeppers,
+            PizzaIngredients.Mushrooms,
+            PizzaIngredients.Olives,
+            PizzaIngredients.Onions,
+            PizzaIngredients.Tomatoes
+        })
+        {
+            Name = "Vegetarian"
+        };
+
+        public static readonly Pizza BBQChicken = new Pizza(new List<PizzaIngredient>()
+        {
+            PizzaIngredients.Chicken,
+            PizzaIngredients.GreenOnions
+        })
+        {
+            Name = "BBQ Chicken",
+            Sauce = PizzaSauce.BBQ            
+        };
+
+        public static readonly Pizza GarlicChicken = new Pizza(new List<PizzaIngredient>()
+        {
+            PizzaIngredients.Chicken,
+            PizzaIngredients.GreenOnions,
+            PizzaIngredients.Mushrooms
+        })
+        {
+            Name = "Garlic Chicken",
+            Sauce = PizzaSauce.White
+        };
+
+        public static readonly Pizza Pesto = new Pizza(new List<PizzaIngredient>()
+        {
+            PizzaIngredients.Tomatoes,
+            PizzaIngredients.Garlic
+        })
+        {
+            Name = "Pesto Pizza",
+            Sauce = PizzaSauce.Pesto
+        };
+    }
+}
