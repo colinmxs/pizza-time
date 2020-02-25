07af8afd67af0ef64fcbf8fb6b8dba5b483cb378
Colin Smith <smith.colin00@gmail.com>
2020-02-24 23:24:18 -0700

    add pizzasizes

---
+++ src/PizzaTime/PizzaTime.Core/Food/PizzaSizes.cs
@@ -0,0 +1,31 @@
+﻿namespace PizzaTime.Core.Food
+{
+    using PizzaTime.Core.Food.Core;
+
+    public static class PizzaSizes
+    {
+        public static readonly PizzaSize Small = new PizzaSize
+        {
+            CostMultiplier = 5.0M,
+            Name = "Small"
+        };
+
+        public static readonly PizzaSize Medium = new PizzaSize
+        {
+            CostMultiplier = 7.50M,
+            Name = "Medium"
+        };
+
+        public static readonly PizzaSize Large = new PizzaSize
+        {
+            CostMultiplier = 10.0M,
+            Name = "Large"
+        };
+
+        public static readonly PizzaSize ExtraLarge = new PizzaSize
+        {
+            CostMultiplier = 15.0M,
+            Name = "Extra Large"
+        };
+    }
+}
