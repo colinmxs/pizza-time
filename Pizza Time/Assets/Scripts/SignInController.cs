602a020ddb1ca9e49cbab50bc2febc44514af1df
Colin Smith <smith.colin00@gmail.com>
2020-03-18 17:59:13 -0600

    wip

---
+++ Pizza Time/Assets/Scripts/SignInController.cs
@@ -0,0 +1,72 @@
+﻿using PizzaTime.Aws.Sdk.Cognito;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignInController : MonoBehaviour
+{
+    public InputField UsernameField;
+    public InputField PasswordField;
+
+    private readonly AuthService _authService = new AuthService();
+
+    public void SignIn() 
+    {
+        var un = UsernameField.text;
+        var pw = PasswordField.text;
+
+        Debug.Log("Sign In");
+        Debug.Log($"UN: {un}");
+        Debug.Log($"PW: {pw}");
+        StartCoroutine(SignIn(un, pw));
+    }
+
+    IEnumerator SignIn(string un, string pw)
+    {
+        _authService.Username = un;
+        _authService.Password = pw;
+        string token = "";
+
+        var thread = new System.Threading.Thread(() => {
+            token = _authService.SignIn().Result;
+        });
+        
+        thread.Start();
+        
+        while (thread.IsAlive)
+        {
+            yield return null;
+        }
+
+        Debug.Log($"Access Token: {token}");
+    }
+
+    public void Register()
+    {
+        var un = UsernameField.text;
+        var pw = PasswordField.text;
+        StartCoroutine(Register(un, pw));
+    }
+
+    IEnumerator Register(string un, string pw)
+    {
+        _authService.Username = un;
+        _authService.Password = pw;       
+
+        string token = "";
+
+        var thread = new System.Threading.Thread(() => {
+            token = _authService.Register().Result;
+        });
+
+        thread.Start();
+
+        while (thread.IsAlive)
+        {
+            yield return null;
+        }
+
+        Debug.Log($"Access Token: {token}");
+    }
+}
